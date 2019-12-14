8d0e9244b60ce463a602fdbf2dbe44fca5a033dd
Brent <mina512@live.com>
2019-12-14 12:23:32 -0600

    Added dynamic sequelize config using env

--- config/config.js
+++ config/config.js
@@ -1,7 +1,9 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": "password",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
     "database": "epic_adventure",
     "host": "127.0.0.1",
     "dialect": "mysql"
