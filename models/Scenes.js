9e8a7bbc5fe53155b327aa14c8e7db665e8a65bc
Brent <mina512@live.com>
2019-12-17 20:43:51 -0600

    Added Scenes table for scenarios

---
+++ models/Scenes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+  const Scene = sequelize.define("sceneData",
+    {
+        scenes: DataTypes.TEXT
+    }
+  );
+  Scene.associate = function(_models) {
+    // associations can be defined here
+  };
+  return Scene;
+};
