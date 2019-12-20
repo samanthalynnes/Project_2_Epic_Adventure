05a0589769f962579de52534229657004fad527e
Samantha Lynnes <samanthalynnes522@gmailcom>
2019-12-19 21:37:12 -0600

    connected to jawsdb for heroku deploy

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": "epic_adventure",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "epic_adventure",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "testdb",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "testdb",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
-  "production": {
-    "use_env_variable": "PLACE_DB_ENV_HERE"
+  production: {
+    use_env_variable: "PLACE_DB_ENV_HERE"
   }
-}
+};;
