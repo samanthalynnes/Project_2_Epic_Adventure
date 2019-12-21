279876f1509e04d07cf404d127b1caab8a99c4be
Brent <mina512@live.com>
2019-12-21 10:39:40 -0600

    Updated seeds with story text and updated scenes table to include response columns

--- models/Scenes.js
+++ models/Scenes.js
@@ -4,7 +4,12 @@ module.exports = (sequelize, DataTypes) => {
   const Scene = sequelize.define("Scene", {
     scenes: DataTypes.TEXT,
     choiceA: DataTypes.STRING,
-    choiceB: DataTypes.STRING
+    choiceB: DataTypes.STRING,
+    responseA: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    responseB: DataTypes.TEXT
   });
   Scene.associate = function(_models) {
     // associations can be defined here
