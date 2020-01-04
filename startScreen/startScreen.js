acce4c6651e5204d429647d1c380d95bda4788c0
Broc <broc.davis@yahoo.com>
2020-01-03 19:26:30 -0600

    file cleanup and removal of handlebars

--- startScreen/startScreen.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("keyup", ev => {
-  if (ev.keyCode === 38) {
-    //YOUR CODE HERE
-
-    //example below
-    document.body.innerHTML = "<h1 id='gameOn'>GAME ON!!!</h1>";
-  }
-});
